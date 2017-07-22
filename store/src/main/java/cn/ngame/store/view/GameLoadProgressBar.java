ca38c8d0088b547fee551382d355826c01448983
liguoliang777 <157308001@qq.com>
2017-07-22 14:20:57 +0800

    修改下载进度条.

--- store/src/main/java/cn/ngame/store/view/GameLoadProgressBar.java
+++ store/src/main/java/cn/ngame/store/view/GameLoadProgressBar.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -19,6 +20,8 @@ import cn.ngame.store.core.utils.CommonUtil;
 public class GameLoadProgressBar extends View {
 
     private static final String TAG = GameLoadProgressBar.class.getSimpleName();
+    private final int mRadius;
+    private final int openColor;
 
     private GameFileStatus gameFileStatus;
     private FileLoadInfo fileLoadInfo;
@@ -30,60 +33,69 @@ public class GameLoadProgressBar extends View {
     private String text;                //进度条上的文字
     private float textSize;             //文字大小
     private float textColor;            //文字色彩
-    private int frontColor = 0;         //进度条前景色
-    private int backgroundColor = 0;    //进度条背景色
+    private int nomalColor = 0;         //进度条前景色
+    private int downloadedColor = 0;    //进度条背景色
     private float width;                //控件宽度
     private float height;               //控件长度
 
     private Paint paint;
+    private RectF mRadiusRect;
 
     public GameLoadProgressBar(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public GameLoadProgressBar(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        backgroundColor = context.getResources().getColor(R.color.download_bg2);
+        downloadedColor = context.getResources().getColor(R.color.download_bg2);
+        mRadius = CommonUtil.dip2px(context, 4);
         //获取布局文件中的值
-        text = context.getResources().getString(attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android","text",0));
-        String tempSize = attrs.getAttributeValue("http://schemas.android.com/apk/res/android","textSize");
-        textSize = CommonUtil.dip2px(context,Float.valueOf(tempSize.substring(0,tempSize.length()-2)));
-        String tempWidth = attrs.getAttributeValue("http://schemas.android.com/apk/res/android","layout_width");
-        width = CommonUtil.dip2px(context,Float.valueOf(tempWidth.substring(0,tempWidth.length()-3)));
-        String tempHeight = attrs.getAttributeValue("http://schemas.android.com/apk/res/android","layout_height");
-        height = CommonUtil.dip2px(context,Float.valueOf(tempHeight.substring(0,tempHeight.length()-3)));
+        text = context.getResources().getString(attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android",
+                "text", 0));
+        String tempSize = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "textSize");
+        textSize = CommonUtil.dip2px(context, Float.valueOf(tempSize.substring(0, tempSize.length() - 2)));
+        String tempWidth = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "layout_width");
+        width = CommonUtil.dip2px(context, Float.valueOf(tempWidth.substring(0, tempWidth.length() - 3)));
+        String tempHeight = attrs.getAttributeValue("http://schemas.android.com/apk/res/android", "layout_height");
+        height = CommonUtil.dip2px(context, Float.valueOf(tempHeight.substring(0, tempHeight.length() - 3)));
 
-        textColor = context.getResources().getColor(attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android","textColor",-1));
-        //frontColor = context.getResources().getColor(attrs.getAttributeResourceValue("http://schemas.android.com/apk/res/android","background",-1));
-        frontColor = context.getResources().getColor(R.color.colorPrimary);
+        textColor = context.getResources().getColor(attrs.getAttributeResourceValue("http://schemas.android" +
+                ".com/apk/res/android", "textColor", -1));
+        //nomalColor = context.getResources().getColor(attrs.getAttributeResourceValue("http://schemas.android
+        // .com/apk/res/android","background",-1));
+        nomalColor = context.getResources().getColor(R.color.download_bt_nomal);
+        openColor = context.getResources().getColor(R.color.download_bt_nomal);
 
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DownLoadProgressBar);
         //progress = typedArray.getInt(R.styleable.DownLoadProgressBar_progress, 0);
         typedArray.recycle();
 
         paint = new Paint();
+        paint.setAntiAlias(true);
 
     }
 
     /**
      * 设置进度条状态
+     *
      * @param status
      */
-    public void setLoadState(GameFileStatus status){
+    public void setLoadState(GameFileStatus status) {
 
         this.gameFileStatus = status;
 
-        if (gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL){
+        if (gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL) {
             text = "下载";
-        } else if(gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD){         //下载中
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD) {         //下载中
             text = "暂停";
-        }else if (gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE){        //已经暂停
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE) {        //已经暂停
             text = "继续";
-        }else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD){ //已经下载完成
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD) { //已经下载完成
             //progress = 100;
             text = "安装";
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL || gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD){     //已经安装
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL || gameFileStatus.getStatus() ==
+                GameFileStatus.STATE_HAS_INSTALL_OLD) {     //已经安装
             text = "打开";
         }
 //        else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD){
@@ -92,12 +104,13 @@ public class GameLoadProgressBar extends View {
         invalidate();
     }
 
-    public void setFileLoadInfo(FileLoadInfo info){
+    public void setFileLoadInfo(FileLoadInfo info) {
         this.fileLoadInfo = info;
     }
 
     /**
      * 根据进度条被点击后状态发生的变化，做响应处理
+     *
      * @param listener
      */
     public void setOnStateChangeListener(OnStateChangeListener listener) {
@@ -106,19 +119,21 @@ public class GameLoadProgressBar extends View {
 
     /**
      * 切换ProgressBar状态
+     *
      * @return state 进度条当前的状态
      */
     public void toggle() {
 
-        if (gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL){
+        if (gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL) {
             startDownload();
-        } else if(gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD){ //下载中，点击后暂停
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD) { //下载中，点击后暂停
             pauseDownload();
-        }else if (gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE){    //暂停中，点击后下载
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE) {    //暂停中，点击后下载
             restartDownload();
-        }else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD){ //已经下载完成，点击后安装
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD) { //已经下载完成，点击后安装
             installApp();
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL || gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD){ //已经安装，点击后打开APP
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL || gameFileStatus.getStatus() ==
+                GameFileStatus.STATE_HAS_INSTALL_OLD) { //已经安装，点击后打开APP
             openApp();
         }
 //        else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD){   //已经安装了旧版本
@@ -130,14 +145,14 @@ public class GameLoadProgressBar extends View {
     /**
      * 进入下载状态
      */
-    private void startDownload(){
-        if(gameFileStatus == null){
+    private void startDownload() {
+        if (gameFileStatus == null) {
             return;
         }
         gameFileStatus.setStatus(GameFileStatus.STATE_DOWNLOAD);
         this.text = "暂停";
         this.invalidate();
-        if(listener != null){
+        if (listener != null) {
             listener.onStartDownload(fileLoadInfo);
         }
     }
@@ -145,12 +160,12 @@ public class GameLoadProgressBar extends View {
     /**
      * 更新App
      */
-    private void updateApp(){
+    private void updateApp() {
 
         gameFileStatus.setStatus(GameFileStatus.STATE_DOWNLOAD);
         this.text = "暂停";
         this.invalidate();
-        if(listener != null){
+        if (listener != null) {
             listener.updateApp(fileLoadInfo);
         }
     }
@@ -158,11 +173,11 @@ public class GameLoadProgressBar extends View {
     /**
      * 由暂停重新开始下载
      */
-    private void restartDownload(){
+    private void restartDownload() {
         gameFileStatus.setStatus(GameFileStatus.STATE_DOWNLOAD);
         this.text = "暂停";
         this.invalidate();
-        if(listener != null){
+        if (listener != null) {
             listener.onRestartDownload(fileLoadInfo);
         }
     }
@@ -170,11 +185,11 @@ public class GameLoadProgressBar extends View {
     /**
      * 暂停下载
      */
-    private void pauseDownload(){
+    private void pauseDownload() {
         gameFileStatus.setStatus(GameFileStatus.STATE_PAUSE);
         this.text = "继续";
         this.invalidate();
-        if(listener != null){
+        if (listener != null) {
             listener.onPauseDownload(fileLoadInfo);
         }
     }
@@ -182,20 +197,19 @@ public class GameLoadProgressBar extends View {
     /**
      * 安装下载文件
      */
-    private void installApp(){
+    private void installApp() {
         gameFileStatus.setStatus(GameFileStatus.STATE_HAS_INSTALL);
         text = "安装";
         invalidate();
-        if(listener != null){
+        if (listener != null) {
             listener.onInstallApp(fileLoadInfo);
         }
     }
 
-    private void openApp(){
-
+    private void openApp() {
         gameFileStatus.setStatus(GameFileStatus.STATE_HAS_INSTALL);
         text = "打开";
-        if(listener != null){
+        if (listener != null) {
             listener.onOpenApp(fileLoadInfo);
             invalidate();
         }
@@ -204,99 +218,119 @@ public class GameLoadProgressBar extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        if(gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL){
+        //RectF对象
+        mRadiusRect = new RectF();
+        mRadiusRect.left = 0;                                 //左边
+        mRadiusRect.top = 0;                                  //上边
+        mRadiusRect.right = (int) width;                       //右边
+        mRadiusRect.bottom = (int) height;                     //下边*/
+        if (gameFileStatus == null || gameFileStatus.getStatus() == GameFileStatus.STATE_UN_INSTALL) {
             //绘制背景色
-            paint.setColor(frontColor);
+            paint.setColor(nomalColor);
             paint.setAntiAlias(true);
-            /*RectF r1=new RectF();                      //RectF对象
-            r1.left=0;                                 //左边
-            r1.top=0;                                  //上边
-            r1.right=(int)width;                       //右边
-            r1.bottom=(int)height;                     //下边*/
-            //canvas.drawRoundRect(r1,15,15,paint);
-
-            canvas.drawRect(0,0,width,height,paint); //绘制矩形
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            //canvas.drawRect(0,0,width,height,paint); //绘制矩形
 
             //绘制字体
-            paint.setColor((int)textColor);
+            paint.setColor((int) textColor);
             paint.setTextSize(textSize);
             paint.setTextAlign(Paint.Align.CENTER);
             Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-            float baseline = (height-fontMetrics.bottom-fontMetrics.top)/2;   //设置字体基线
-            canvas.drawText(text, width/2, baseline, paint);
+            float baseline = (height - fontMetrics.bottom - fontMetrics.top) / 2;   //设置字体基线
+            canvas.drawText(text, width / 2, baseline, paint);
 
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD){
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL_OLD) {
 
             //绘制背景色
-            paint.setColor(frontColor);
-            paint.setColor(backgroundColor);
-            canvas.drawRect(0,0,width,height,paint);
+            paint.setColor(nomalColor);
+            paint.setColor(downloadedColor);
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            // canvas.drawRect(0, 0, width, height, paint);
 
             //绘制字体
-            paint.setColor((int)textColor);
+            paint.setColor((int) textColor);
             paint.setTextSize(textSize);
             paint.setTextAlign(Paint.Align.CENTER);
             Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-            float baseline = (height-fontMetrics.bottom-fontMetrics.top)/2;   //设置字体基线
-            canvas.drawText(text, width/2, baseline, paint);
+            float baseline = (height - fontMetrics.bottom - fontMetrics.top) / 2;   //设置字体基线
+            canvas.drawText(text, width / 2, baseline, paint);
 
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD) {
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_DOWNLOAD) {
+            mRadiusRect = new RectF();
+            mRadiusRect.left = 0;                                 //左边
+            mRadiusRect.top = 0;                                  //上边
+            mRadiusRect.right = (int) width;                       //右边
+            mRadiusRect.bottom = (int) height;                     //下边*/
+            //绘制背景色
+            paint.setColor(downloadedColor);
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            //canvas.drawRect(0, 0, width, height, paint);    //绘背景
+
+            paint.setColor(nomalColor);
+            int downingWidth = (int) (width / gameFileStatus.getLength() * gameFileStatus.getFinished());
+            mRadiusRect.right = downingWidth;
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            // canvas.drawRect(0, 0,  downingWidth, height, paint);
+            //绘前景
+
+            //绘制字体
+            paint.setColor((int) textColor);
+            paint.setTextSize(textSize);
+            paint.setTextAlign(Paint.Align.CENTER);
+            Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
+            float baseline = (height - fontMetrics.bottom - fontMetrics.top) / 2;   //设置字体基线
+
+            canvas.drawText(text, width / 2, baseline, paint);
+
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE) {
 
             //绘制背景色
-            paint.setColor(backgroundColor);
-            canvas.drawRect(0,0,width,height,paint);    //绘背景
+            paint.setColor(downloadedColor);
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            //canvas.drawRect(0, 0, width, height, paint);    //绘背景
 
-            paint.setColor(frontColor);
-            canvas.drawRect(0,0,(int)(width/gameFileStatus.getLength()*gameFileStatus.getFinished()),height,paint);   //绘前景
+            paint.setColor(nomalColor);
+            int pauseWidth = (int) (width / gameFileStatus.getLength() * gameFileStatus.getFinished());
+            mRadiusRect.right = pauseWidth;
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paint);
+            // canvas.drawRect(0, 0,  pauseWidth, height, paint);
+            //绘前景
 
             //绘制字体
-            paint.setColor((int)textColor);
+            paint.setColor((int) textColor);
             paint.setTextSize(textSize);
             paint.setTextAlign(Paint.Align.CENTER);
             Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-            float baseline = (height-fontMetrics.bottom-fontMetrics.top)/2;   //设置字体基线
+            float baseline = (height - fontMetrics.bottom - fontMetrics.top) / 2;   //设置字体基线
+            canvas.drawText(text, width / 2, baseline, paint);
 
-            canvas.drawText(text, width/2, baseline, paint);
-
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_PAUSE){
-
+        } else if (gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD || gameFileStatus.getStatus() ==
+                GameFileStatus.STATE_HAS_INSTALL) {  //如以下载完成 或已成功安装
+          /*  Paint.Style.FILL :填充内部
+            Paint.Style.FILL_AND_STROKE ：填充内部和描边
+            Paint.Style.STROKE ：仅描边*/
             //绘制背景色
-            paint.setColor(backgroundColor);
-            canvas.drawRect(0,0,width,height,paint);    //绘背景
-
-            paint.setColor(frontColor);
-            canvas.drawRect(0,0,(int)(width/gameFileStatus.getLength()*gameFileStatus.getFinished()),height,paint);   //绘前景
+            Paint paintStoke = new Paint();
+            paintStoke.setStyle(Paint.Style.STROKE );
+            paintStoke.setColor(openColor);
+            paintStoke.setStrokeWidth(2);
+            canvas.drawRoundRect(mRadiusRect, mRadius, mRadius, paintStoke);//圆角
+            //canvas.drawRect(0, 0, width, height, paint);
 
             //绘制字体
-            paint.setColor((int)textColor);
+            paint.setColor(openColor);
             paint.setTextSize(textSize);
             paint.setTextAlign(Paint.Align.CENTER);
             Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-            float baseline = (height-fontMetrics.bottom-fontMetrics.top)/2;   //设置字体基线
-            canvas.drawText(text, width/2, baseline, paint);
-
-        }else if(gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_DOWNLOAD || gameFileStatus.getStatus() == GameFileStatus.STATE_HAS_INSTALL){  //如以下载完成 或已成功安装
-
-            //绘制背景色
-            paint.setColor(frontColor);
-            paint.setColor(backgroundColor);
-            canvas.drawRect(0,0,width,height,paint);
-
-            //绘制字体
-            paint.setColor((int)textColor);
-            paint.setTextSize(textSize);
-            paint.setTextAlign(Paint.Align.CENTER);
-            Paint.FontMetricsInt fontMetrics = paint.getFontMetricsInt();
-            float baseline = (height-fontMetrics.bottom-fontMetrics.top)/2;   //设置字体基线
-            canvas.drawText(text, width/2, baseline, paint);
+            float baseline = (height - fontMetrics.bottom - fontMetrics.top) / 2;   //设置字体基线
+            canvas.drawText(text, width / 2, baseline, paint);
         }
     }
 
     /**
      * 进度条完成后执行的操作
      */
-    public interface OnStateChangeListener{
+    public interface OnStateChangeListener {
 
         /**
          * 处理开始下载任务
@@ -307,18 +341,22 @@ public class GameLoadProgressBar extends View {
          * 已安装了旧版本，执行更新操作
          */
         void updateApp(FileLoadInfo info);
+
         /**
          * 处理暂停操作
          */
         void onPauseDownload(FileLoadInfo info);
+
         /**
          * 处理暂停后重新下载操作
          */
         void onRestartDownload(FileLoadInfo info);
+
         /**
          * 处理下载完成后安装APP的操作
          */
         void onInstallApp(FileLoadInfo info);
+
         /**
          * 处理APP已安装成功后的打开操作
          */
