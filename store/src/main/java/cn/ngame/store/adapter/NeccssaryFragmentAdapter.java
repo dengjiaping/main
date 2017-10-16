b68a79e1c142002d224f8772b6e56e42908d8749
liguoliang7 <futuretaskgoollee@yeah.net>
2017-10-16 10:08:59 +0800

    1.优化

--- store/src/main/java/cn/ngame/store/adapter/NeccssaryFragmentAdapter.java
+++ store/src/main/java/cn/ngame/store/adapter/NeccssaryFragmentAdapter.java
@@ -215,7 +215,7 @@ public class NeccssaryFragmentAdapter extends BaseAdapter implements StickyListH
                 }
             };
             timerTasks.add(task);
-            timer.schedule(task, 0, 300);
+            timer.schedule(task, 0, 500);
         }
 
         public void update(final NecessaryItemData toolInfo) {
@@ -238,11 +238,9 @@ public class NeccssaryFragmentAdapter extends BaseAdapter implements StickyListH
                     int lineCount = tv_desc.getLineCount();
                     if (1 == lineCount) {
                         tv_desc.setSingleLine(false);
-                        show_more_disc_bt.setVisibility(View.INVISIBLE);
                         show_more_disc_bt.setImageResource(R.drawable.ic_bottom_hide_more);
                     } else {
                         tv_desc.setMaxLines(1);
-                        show_more_disc_bt.setVisibility(View.VISIBLE);
                         show_more_disc_bt.setImageResource(R.drawable.ic_bottom_show_more);
                     }
                 }
